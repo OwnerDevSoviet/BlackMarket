a9a4ff70a2a480e0bb1d16fb80ecd744b117d595
OwnerDevSoviet <70889124+OwnerDevSoviet@users.noreply.github.com>
2020-09-14 01:55:48 -0700

    Add files via upload

---
+++ Blackmarket/config.lua
@@ -0,0 +1,24 @@
+Config         = {}
+
+--- PRICING FOR EACH SECTION
+Config.UsbPrice      = 1000
+Config.laptopPrice   = 25000
+
+-- LOCATIONS OF EACH PLACE
+Config.Smuggler  = {x = 1269.17, y = -2561.02, z = 41.92}
+Config.Dealer   = {x = 1247.84, y = -2578.52, z = 41.93}
+Config.Hustler  = {x = 1257.06, y = -2570.63, z = 41.73}
+Config.Desk     = {x = 1275.72, y = -1710.55, z = 54.77}
+Config.Final     = {x = -1270.66, y = -3398.63, z = 13.94}
+Config.ArmsDealer     = {x = 1258.91, y = -2558.33, z = 41.72}
+--- WEAPONS LISTED WITHIN THE RANDOM CHANCE PRIZES
+Config.One = 'WEAPON_PISTOL50'
+Config.Two = 'WEAPON_PISTOL'
+Config.Three = 'WEAPON_MUSKET'
+Config.Four = 'WEAPON_ASSAULTRIFLE'
+Config.Five = 'WEAPON_COMPACTRIFLE'
+Config.Six = 'WEAPON_MICROSMG'
+Config.Seven = 'WEAPON_COMPACTRIFLE'
+Config.Eight = 'WEAPON_MINISMG'
+Config.Nine = 'WEAPON_SMG'
+Config.Ten = 'WEAPON_PISTOL'
