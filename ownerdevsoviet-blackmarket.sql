435400b029b769c98c04db1735f6910ad1986ec2
OwnerDevSoviet <70889124+OwnerDevSoviet@users.noreply.github.com>
2020-09-14 17:55:48 -0700

    Update ownerdevsoviet-blackmarket.sql

--- ownerdevsoviet-blackmarket.sql
+++ ownerdevsoviet-blackmarket.sql
@@ -1,5 +1,5 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
 ('usbstick', 'encrypted USB', -1, 0, 1),
-('usbgolden', 'Decrypted USB', 2, 0, 1),
+('usbgolden', 'Decrypted Data', 2, 0, 1),
 ('laptop', 'Decrypter Laptop', -1, 0, 1),
 ('weaponcase', 'Black Weapon Case', 2, 0, 1);
